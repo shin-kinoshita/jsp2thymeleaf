72505fad3574ece40d5d6395351b2ac8bf76981a
shin-kinoshita <kinoshita116@gmail.com>
2018-07-21 00:07:47 +0900

    refactor

---
+++ replace_definition/__init__.py
@@ -0,0 +1,7 @@
+from replace_definition.basic.if_def import IfDef
+from replace_definition.basic.foreach_def import ForeachDef
+
+def_list = [
+    IfDef,
+    ForeachDef,
+]
--- transform/transform_tags.py
+++ transform/transform_tags.py
@@ -1,26 +1,20 @@
 from bs4 import BeautifulSoup, Tag
 import re
 
-from replace_definition.if_def import IfDef
-from replace_definition.foreach_def import ForeachDef
-
-DEF_LIST = [
-    IfDef(),
-    ForeachDef(),
-]
+from replace_definition import def_list
 
 
 def transform_tags(in_file, out_file):
     soup = BeautifulSoup(in_file, "html.parser")
 
-    for def_obj in DEF_LIST:
+    for def_obj in def_list:
         tag_list = soup.find_all(
-            name=re.compile("^" + def_obj.search_name()),
-            attrs=def_obj.search_attrs(),
-            text=def_obj.search_text()
+            name=re.compile("^" + def_obj.search_name(def_obj)),
+            attrs=def_obj.search_attrs(def_obj),
+            text=def_obj.search_text(def_obj)
         )
         for tag in tag_list:
-            def_obj.operation(soup, tag)
+            def_obj.operation(def_list, soup, tag)
         soup = BeautifulSoup(soup.renderContents())
 
     out_file.write(soup.prettify("utf-8"))
