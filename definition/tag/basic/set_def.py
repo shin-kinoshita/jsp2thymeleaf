86cb4c373e4b0fbceeeb17a4ae82c9dcc6a0ab0c
shin-kinoshita <kinoshita116@gmail.com>
2018-07-24 00:24:53 +0900

    remove bug on set def

--- definition/tag/basic/set_def.py
+++ definition/tag/basic/set_def.py
@@ -1,3 +1,5 @@
+import re
+
 from bs4 import Tag
 
 from definition.tag.abs_tag_def import AbsTagDef
@@ -18,7 +20,7 @@ class SetDef(AbsTagDef):
             var_val = old_tag["var"]
             value_val = old_tag["value"]
 
-            if not str.isnumeric(value_val):
+            if not str.isnumeric(value_val) and not re.match(".*\$\{.*\}.*", value_val):
                 value_val = "'{0}'".format(value_val)
             new_tag = Tag(parser, name="div", attrs=[("th:with", "{0}={1}".format(var_val, value_val))])
             new_tag.contents = old_tag.contents
