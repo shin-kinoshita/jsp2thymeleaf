5074f11fbbea47ba071107e6437669f95ea6e637
shin-kinoshita <kinoshita116@gmail.com>
2018-07-24 01:17:59 +0900

    modify __init__.py of tag package for custom tag definition

--- definition/tag/__init__.py
+++ definition/tag/__init__.py
@@ -1,3 +1,4 @@
 from .basic import tag_def_list as basic_tag_def_list
+from .custom import tag_def_list as custom_tag_def_list
 
-tag_def_list = basic_tag_def_list
+tag_def_list = basic_tag_def_list + custom_tag_def_list
